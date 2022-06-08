1dd6f933ee31a66d3cc6c0270284c51f68ef98fe
Matheus <matheusbj95@gmail.com>
2022-06-08 20:31:23 -0300

    teste

---
+++ TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/Ti20Lp2ApiEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lp2.fundatec.TI20LP2APIestacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ti20Lp2ApiEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ti20Lp2ApiEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ TI20-LP2-API-estacionamento/src/test/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/Ti20Lp2ApiEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lp2.fundatec.TI20LP2APIestacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ti20Lp2ApiEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
