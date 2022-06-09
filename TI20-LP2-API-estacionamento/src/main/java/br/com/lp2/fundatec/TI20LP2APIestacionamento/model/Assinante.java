34399b861c1cad4016563406bfc93173c7bbf819
Matheus <matheusbj95@gmail.com>
2022-06-08 21:56:35 -0300

    deletado classe Plano

--- TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/model/Assinante.java
+++ TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/model/Assinante.java
@@ -1,10 +1,14 @@
 package br.com.lp2.fundatec.TI20LP2APIestacionamento.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
@@ -25,9 +29,9 @@ public class Assinante {
 	@Column(name = "telefone")
 	private String telefone;
 	
-	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "id_endereco", referencedColumnName = "id")
 	private Endereco endereco;
 
 	
-	private Plano plano;
 }
