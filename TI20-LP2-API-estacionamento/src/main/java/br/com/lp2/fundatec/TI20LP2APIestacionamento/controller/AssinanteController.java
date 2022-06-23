3b9994b0d0bfca34e81663b57a514ab241884c3c
PauloGabriel347 <paulogabriel347@gmail.com>
2022-06-22 22:06:01 -0300

    Criação de metodos em Classes do Assinante

--- TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/controller/AssinanteController.java
+++ TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/controller/AssinanteController.java
@@ -1,4 +1,26 @@
 package br.com.lp2.fundatec.TI20LP2APIestacionamento.controller;
 
+import br.com.lp2.fundatec.TI20LP2APIestacionamento.model.Assinante;
+import br.com.lp2.fundatec.TI20LP2APIestacionamento.service.AssinanteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/assinante")
 public class AssinanteController {
+    private final AssinanteService assinanteService;
+
+    public AssinanteController(AssinanteService assinanteService){this.assinanteService = assinanteService; }
+
+    @GetMapping("/{nome}")
+    public Assinante findAssinanteByNome(@PathVariable String nome){
+        Optional<Assinante> optionalAssinante = assinanteService.findByNome(nome);
+        if (optionalAssinante.isPresent())
+            return optionalAssinante.get();
+        return null;
+    }
 }
--- TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/repository/AssinanteRepository.java
+++ TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/repository/AssinanteRepository.java
@@ -4,7 +4,11 @@ import br.com.lp2.fundatec.TI20LP2APIestacionamento.model.Assinante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AssinanteRepository extends JpaRepository<Assinante, Integer>
-{
+public interface AssinanteRepository extends JpaRepository<Assinante, Integer> {
+    Optional<Assinante> findByCPF(String cpf);
+
+    Optional<Assinante> findByNome(String nome);
 }
