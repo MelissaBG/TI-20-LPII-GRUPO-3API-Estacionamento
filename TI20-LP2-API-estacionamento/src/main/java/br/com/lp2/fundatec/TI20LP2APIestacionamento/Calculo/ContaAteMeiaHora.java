e8688b98bd930b5871a969604e42e44a817a4660
melissabgoulart@gmail.com <melissabgoulart@example.com>
2022-07-25 19:03:26 -0300

    Alterado antes da aula de BD

--- TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/Calculo/ContaAteMeiaHora.java
+++ TI20-LP2-API-estacionamento/src/main/java/br/com/lp2/fundatec/TI20LP2APIestacionamento/Calculo/ContaAteMeiaHora.java
@@ -5,7 +5,7 @@ import br.com.lp2.fundatec.TI20LP2APIestacionamento.SegundaVersao.Refatorado.Con
 import br.com.lp2.fundatec.TI20LP2APIestacionamento.SegundaVersao.Refatorado.Enums.TipoTarifa;
 import br.com.lp2.fundatec.TI20LP2APIestacionamento.CodigoAntigo.Tarifa;
 
-public class ContaAteMeiaHoraStrategy implements ContaStrategy {
+public class ContaAteMeiaHora implements ContaStrategy {
     @Override
     public boolean contaEhDoTipoTarifa(Tarifa tarifa){
         return TipoTarifa.ATE_MEIA_HORA == tarifa.getTipoTarifa();
